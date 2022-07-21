dbf7cb011ad197ba28856923144c13b89d7ab0b0
kongguksu <104235938+kongguksu@users.noreply.github.com>
2022-07-21 15:52:31 -0700

    fix classList

--- js/script.js
+++ js/script.js
@@ -6,6 +6,12 @@ let starNum;
 const main1 = document.querySelector(`.main-1`);
 const main2 = document.querySelector(`.main-2`);
 
+// const star = document.querySelector(`.btn-number`);
+// const stars = document.querySelectorAll(`.btn-number`);
+
+const starContainer = document.querySelector(`.numbers`);
+const stars = starContainer.getElementsByTagName(`button`);
+
 const star1 = document.querySelector(`.star-1`);
 const star2 = document.querySelector(`.star-2`);
 const star3 = document.querySelector(`.star-3`);
@@ -19,65 +25,14 @@ const result = document.querySelector(`.result-number`);
 // User clicks rating --- only one rating can have clicked status
 starNum = 0;
 
-star1.addEventListener(`click`, function () {
-  star1.classList.add(`star-clicked`);
-
-  star2.classList.remove(`star-clicked`);
-  star3.classList.remove(`star-clicked`);
-  star4.classList.remove(`star-clicked`);
-  star5.classList.remove(`star-clicked`);
-
-  starNum = 1;
-  return starNum;
-});
-
-star2.addEventListener(`click`, function () {
-  star2.classList.add(`star-clicked`);
-
-  star1.classList.remove(`star-clicked`);
-  star3.classList.remove(`star-clicked`);
-  star4.classList.remove(`star-clicked`);
-  star5.classList.remove(`star-clicked`);
-
-  starNum = 2;
-  return starNum;
-});
-
-star3.addEventListener(`click`, function () {
-  star3.classList.add(`star-clicked`);
-
-  star1.classList.remove(`star-clicked`);
-  star2.classList.remove(`star-clicked`);
-  star4.classList.remove(`star-clicked`);
-  star5.classList.remove(`star-clicked`);
-
-  starNum = 3;
-  return starNum;
-});
-
-star4.addEventListener(`click`, function () {
-  star4.classList.add(`star-clicked`);
-
-  star1.classList.remove(`star-clicked`);
-  star2.classList.remove(`star-clicked`);
-  star3.classList.remove(`star-clicked`);
-  star5.classList.remove(`star-clicked`);
-
-  starNum = 4;
-  return starNum;
-});
-
-star5.addEventListener(`click`, function () {
-  star5.classList.add(`star-clicked`);
-
-  star1.classList.remove(`star-clicked`);
-  star2.classList.remove(`star-clicked`);
-  star3.classList.remove(`star-clicked`);
-  star4.classList.remove(`star-clicked`);
-
-  starNum = 5;
-  return starNum;
-});
+for (let i = 0; i < stars.length; i++) {
+  stars[i].addEventListener(`click`, function () {
+    document.querySelector(`.star-clicked`)
+      ? document.querySelector(`.star-clicked`).classList.remove(`star-clicked`)
+      : ``;
+    this.classList.add(`star-clicked`);
+  });
+}
 
 // User clicks submit -- if rating is clicked
 submit.addEventListener(`click`, function () {
